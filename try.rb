2ed583ba1ba37d9ccc88cb808cb4a41073934f5e
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-24 06:14:06 +0300

    commit tree 1b2a624925a07ff36f5cdd87d2e30ef51bf1d0cc

---
+++ try.rb
@@ -0,0 +1,10 @@
+# # class Ghost
+#     def self.reflect
+#         self
+#         puts "This is self"
+#     end
+#     puts "Outside self"
+# end
+# # g = Ghost.new
+# # g.reflect == g
+# Ghost.reflect == Ghost
